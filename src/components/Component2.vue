52995713fc2b4af9b220c59c589d53864a179c79
Wilbur Zhao <wilbur.zhao@vertogroup.com.au>
2022-07-25 11:38:25 +0930

    Install and Challenge Creation
    @jny986
    jny986 committed on 24 Jan 2020
    
    Initial and challenge creation

---
+++ src/components/Component2.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>{{ msg[0] }}</p>
+        <p>{{ msg[1] }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Compontent2',
+    props: {
+        msg: {
+            require: true,
+            type: Array
+        }
+    }
+}
+</script>
